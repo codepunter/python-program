c58923d63bc2d2fa38da3abed43f0a6482ea130b
codepunter <codepunter@users.noreply.github.com>
2018-08-07 23:54:27 +0530

    Add files via upload

---
+++ WebInstall.bat
@@ -0,0 +1,3 @@
+python WebService.py --startup auto install 
+python InstaMSWordService.py --startup auto install 
+python InstaWebBackendService.py --startup auto install
---
+++ WebRemove.bat
@@ -0,0 +1,3 @@
+python WebService.py stop && python WebService.py remove
+python InstaMSWordService.py stop && python InstaMSWordService.py remove
+python InstaWebBackendService.py stop && python InstaWebBackendService.py remove
